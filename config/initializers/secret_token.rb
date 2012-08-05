0fd5fd54253414bb69e9982e63114940ef54e5cd
naokisoya <ringojam12021@gmail.com>
2012-08-05 15:57:16 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+September::Application.config.secret_token = 'adce3af73cea68a2158362e776fcc2ea0dc0b6088dc0a71cc384a20b87b36f63ece9d9754c3659930b504e3f1ffef277a44963f399bac6b63708b2126ad510a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+September::Application.config.session_store :cookie_store, key: '_september_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# September::Application.config.session_store :active_record_store
